286625df7d61f8663d735917272a5805fcad757c
kykiran68 <kykiran68@gmail.com>
2024-09-10 16:45:42 +0530

    my project

--- practice/python_pract.py
+++
@@ -1,4 +0,0 @@
-import math
-for i in range(1,10):
-    print(math.pow(i,2))
-
--- practice/test_pra.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-
-@pytest.mark.parametrize("user,password",[("abc","123"),("cdb","235"),("cgh","398")])
-def test_name(user,password):
-    print(user,"--",password)
-    print(user+"--"+password)
-    print("Kiran")
